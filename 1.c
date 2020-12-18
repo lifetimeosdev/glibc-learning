f01cb9a495117f38e29f59222d4359002d3bc264
lifetimeosdev <lifetimeosdev@gmail.com>
2020-12-18 21:13:23 +0800

    update

--- 1.c
+++ 1.c
@@ -1,9 +1,19 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
-    printf("%s\n", "hello world");
-    printf("%d\n", errno);
+    printf("errno=%d\n", errno);
+    printf("EACCESS is %s\n", strerror(EACCES));
+
+    char buf[BUFSIZ];
+    printf("EACCESS is %s\n", strerror_r(EACCES, buf, BUFSIZ));
+
+    errno = EACCES;
+    perror("Error is ");
+
     return 0;
 }
