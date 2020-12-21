1c0fd88b4794ca529b74f3d21c3ebc7b344d1c1c
lifetimeosdev <lifetimeosdev@gmail.com>
2020-12-21 23:52:16 +0800

    update

--- 1.c
+++ 1.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
+
+FILE *open_sesame(char *name);
 
 int main(int argc, char **argv)
 {
@@ -18,5 +21,24 @@ int main(int argc, char **argv)
     printf("program invocation name is %s\n", program_invocation_name);
     printf("program invocation short name is %s\n", program_invocation_short_name);
 
+    open_sesame("/tmp/1.txt");
+
     return 0;
+}
+
+FILE *
+open_sesame(char *name)
+{
+    FILE *stream;
+
+    errno = 0;
+    stream = fopen(name, "r");
+    if (stream == NULL)
+    {
+        fprintf(stderr, "%s: Could't open file %s; %s\n",
+                program_invocation_short_name, name, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    else
+        return stream;
 }
