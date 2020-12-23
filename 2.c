b363a9ecc0b563682655e2bc5a5d43ae0981b132
lifetimeosdev <lifetimeosdev@gmail.com>
2020-12-23 20:50:17 +0800

    update 2.c

--- 2.c
+++ 2.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <malloc.h>
+#include <mcheck.h>
 
 struct foo
 {
@@ -9,11 +11,26 @@ struct foo
 
 int main(int argc, char *argv[])
 {
+    //mcheck(NULL);
+
     struct foo *ptr;
     ptr = (struct foo *)malloc(sizeof(struct foo));
     if (ptr == NULL)
         abort();
     memset(ptr, 0, sizeof(struct foo));
+
+    ptr = (struct foo *)realloc(ptr, 2 * sizeof(struct foo));
+    ptr = (struct foo *)reallocarray(ptr, 2, sizeof(struct foo));
+    ptr = (struct foo *)calloc(4, sizeof(struct foo));
+
+    ptr = (struct foo *)aligned_alloc(8, sizeof(struct foo));
+    posix_memalign((void *)&ptr , 8, sizeof(struct foo));
+    enum mcheck_status status = mprobe(ptr);
+    printf("%d",status);
+
+    free(ptr);
+    status = mprobe(ptr);
+    printf("%d",status);
     return 0;
 }
 
