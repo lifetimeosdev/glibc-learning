6c53091667ec17737ea56dc4aac566448f8faa67
lifetimeosdev <lifetimeosdev@gmail.com>
2020-12-24 20:35:07 +0800

    update

--- 2.c
+++ 2.c
@@ -26,11 +26,21 @@ int main(int argc, char *argv[])
     ptr = (struct foo *)aligned_alloc(8, sizeof(struct foo));
     posix_memalign((void *)&ptr , 8, sizeof(struct foo));
     enum mcheck_status status = mprobe(ptr);
-    printf("%d",status);
+    printf("%d\n",status);
 
     free(ptr);
     status = mprobe(ptr);
-    printf("%d",status);
+    printf("%d\n",status);
+
+    struct mallinfo minfo = mallinfo();
+    printf("total size of memory alloced is %d bytes\n", minfo.arena);
+    printf("%d trunks are not in use.\n", minfo.ordblks);
+    printf("%d trunks of memory are alloced by mmap.\n", minfo.hblks);
+    printf("%d bytes of memory are alloced by mmap.\n", minfo.hblkhd);
+    printf("total size of memory occupied by chunks are %d .\n", minfo.uordblks);
+    printf("This is the total size of memory occupied by free (not in use) chunks. %d.\n", minfo.fordblks);
+    printf("This is the size of the top-most releasable chunk that normally borders the end of the heap %d.\n", minfo.keepcost);
+
     return 0;
 }
 
