7df05ac984e33fae23f8865d3d38407d4bb70edc
lifetimeosdev <lifetimeosdev@gmail.com>
2021-02-25 23:01:20 +0800

    update

--- 20.1.c
+++ 20.1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char *argv[])
 {
@@ -12,6 +13,32 @@ int main(int argc, char *argv[])
     lldiv_t llresult = lldiv(10, 2);
     printf("%lld %lld\n", llresult.quot, llresult.rem);
 
+    double a = 1.0 / 3;
+    int c = fpclassify(0.0000);
+    switch (c)
+    {
+    case FP_NAN:
+        printf("NaN");
+        break;
+    case FP_INFINITE:
+        printf("infinity");
+        break;
+    case FP_ZERO:
+        printf("zero");
+        break;
+    case FP_SUBNORMAL:
+        printf("subnormal");
+        break;
+    case FP_NORMAL:
+        printf("normal");
+        break;
+    default:
+        break;
+    }
 
+    printf("\nisfinite %d\n", isfinite(INFINITY));
+
+    printf("cast float %d\n", (int)111.2);
+    printf("cast float %d\n", (int)111.5);
     return 0;
 }
