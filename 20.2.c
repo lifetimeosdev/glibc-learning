7c49311eace1d0d45af0847a8c1dea9e86f863cf
lifetimeosdev <lifetimeosdev@gmail.com>
2021-03-01 22:32:07 +0800

    update

--- 20.2.c
+++ 20.2.c
@@ -3,19 +3,39 @@
 #include <math.h>
 
 int main(int argc, char *argv[])
-{   
+{
     int result = abs(-1);
-    printf("abs result is %d\n",result);
-    
+    printf("abs result is %d\n", result);
+
     double d = fabs(-1.23456789);
-    printf("fabs result is %f\n",d);
+    printf("fabs result is %f\n", d);
 
     float f = fabsf(-1.23456789);
-    printf("fabsf result is %f\n",f);
+    printf("fabsf result is %f\n", f);
 
     long double ld = fabsl(-1.23456789);
-    printf("fabsl result is %Lf\n",ld);
+    printf("fabsl result is %Lf\n", ld);
 
+    double c = ceil(1.1);
+    printf("ceil result is %f\n", c);
 
+    double fl = floor(1.1);
+    printf("floor result is %f\n", fl);
+
+    double t = trunc(1.1);
+    printf("trunc result is %f\n", t);
+
+    double r = rint(1.5);
+    printf("rint result is %f\n", r);
+
+    double ni = nearbyint(-1.5);
+    printf("nearbyint result is %f\n", ni);
+
+    double rd = round(1.5);
+    printf("round result is %f\n", rd);
+
+    int g = isgreater(1.111111111111112,
+                      1.11111111111111);
+    printf("isgreater result is %d\n", g);
     return 0;
 }
