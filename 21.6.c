5bffff3d47c48c8fc557fe24ef1995d51653ad6a
lifetimeosdev <lifetimeosdev@gmail.com>
2021-03-07 10:08:07 +0800

    update

--- 21.6.c
+++ 21.6.c
@@ -1,5 +1,7 @@
+#define _XOPEN_SOURCE
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -17,6 +19,12 @@ int main(int argc, char *argv[])
 
     int ret = strftime(str, sizeof(str), "%Y-%m-%d %H:%M:%S %Z", tm1p);
     printf("%s\n", ta);
+
+    struct tm tm2;
+    memset(&tm2, '\0', sizeof(tm2));
+    strptime(str, "%Y-%m-%d %H:%M:%S %Z", &tm2);
+    printf("%d-%d-%d %d:%d:%d\n", tm2.tm_year + 1900,
+           tm2.tm_mon + 1, tm2.tm_mday, tm2.tm_hour, tm2.tm_min, tm2.tm_sec);
 
     return 0;
 }
